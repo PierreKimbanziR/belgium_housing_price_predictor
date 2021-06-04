9e03f904754a297ef3ab2c42d43b701cdcb5daf6
PierreKimbanziR <kimbanzip@gmail.com>
2021-06-04 13:06:11 +0200

    add streamlit interface

---
+++ main.py
@@ -0,0 +1,136 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
+import os
+import webbrowser
+
+cwd = os.getcwd()  # Get the current working directory (cwd)
+model = pickle.load(open(cwd + "/estimator.pkl", "rb"))
+
+
+data = pd.read_csv(
+    "https://raw.githubusercontent.com/SamuelD005/challenge-regression/development/Data8.csv",
+    sep=",",
+)
+x = data.drop(["Price", "PriceperMeter", "Unnamed: 0"], axis=1)
+columns = x.columns
+
+st.title("Estimate the price of your property")
+
+st.subheader("A new approach to real-estate prices definition")
+
+
+def main():
+    st.write(
+        """
+        #### To evaluate the price of your property we will need a few infomations.  
+        """
+    )
+    type_of_property = st.selectbox(
+        "What type of property do you want to estimate ? ", ("--", "house", "apartment")
+    )
+    locality = st.number_input(
+        "Please enter the property's postal code.", min_value=1000, max_value=9999
+    )
+    region = st.selectbox(
+        "Select th region of the property", ("Flanders", "Wallonia", "Brussel")
+    )
+    province = st.selectbox(
+        "Select the province of the property",
+        options=[
+            "------",
+            "Flandre Occidental",
+            "Flandre Oriental",
+            "Hainaut",
+            "Brussel",
+            "Anvers",
+            "Liège",
+            "Brabant Flamand",
+            "Brabant Wallon",
+            "Limbourg",
+            "Namur",
+            "Luxembourg",
+        ],
+    )
+
+    area = st.number_input(
+        "Please enter the area (in m²) of the property.", min_value=0, max_value=1000
+    )
+    number_of_room = st.select_slider(
+        "how many rooms there are in the property",
+        options=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    )
+    state_of_building = st.select_slider(
+        "what state the property is in",
+        options=["to renovate", "medium", "good", "new"],
+    )
+    st.write("_____________________________________________________")
+    fully_equipped_kitchen = st.selectbox(
+        "Is your Kitchen fully equipped?", ["No", "Yes"]
+    )
+    furnished = st.selectbox("Is your house is sell furnished?", ["No", "Yes"])
+    open_fire = st.selectbox("Do you have an open fire?", ["No", "Yes"])
+    st.write("_____________________________________________________")
+
+    terrace_area = st.number_input(
+        "Enter the area of your terrace", min_value=0, max_value=1000
+    )
+    garden_area = st.number_input(
+        "Enter the area of your garden", min_value=0, max_value=100000000
+    )
+    number_of_facades = st.selectbox("What is the number of facades?", [2, 3, 4])
+    swimming_pool = st.selectbox("Do you have a swimming pool?", ["No", "Yes"])
+    surface_of_the_land = area + terrace_area + garden_area
+
+    fully_equipped_kitchen = 1 if fully_equipped_kitchen == "Yes" else 0
+    furnished = 1 if furnished == "Yes" else 0
+    open_fire = 1 if open_fire == "Yes" else 0
+    swimming_pool = 1 if swimming_pool == "Yes" else 0
+
+    df = pd.DataFrame(
+        [
+            [
+                locality,
+                type_of_property,
+                number_of_room,
+                area,
+                fully_equipped_kitchen,
+                furnished,
+                open_fire,
+                terrace_area,
+                garden_area,
+                surface_of_the_land,
+                number_of_facades,
+                swimming_pool,
+                state_of_building,
+                province,
+                region,
+            ]
+        ],
+        columns=columns,
+    )
+
+    with st.sidebar:
+        lkd = "https://www.linkedin.com/in/pierre-kimbanzi-ruganda/"
+        gth = "https://github.com/PierreKimbanziR"
+        st.header("Belgium real-estate price estimation")
+        """
+        
+        
+        ### Links
+
+        
+        """
+        if st.button("Github repository"):
+            webbrowser.open(gth)
+        if st.button("Find me on linkedin"):
+            webbrowser.open(lkd)
+
+    if st.button("Estimate the property"):
+        result = model.predict(df)
+        st.success(f"The estimated price of the property is {result[0]}")
+
+
+if __name__ == "__main__":
+    main()
